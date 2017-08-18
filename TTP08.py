a63c04e985f787069cb8eaf1ae8d7d1e313e72f2
bepanther2 <ngakana@icloud.com>
2017-08-18 15:34:25 +0200

    Adding TTP08.py file

---
+++ TTP08.py
@@ -0,0 +1,16 @@
+for letter in 'Python':
+    if letter == 'h':
+        break
+    print('Current Letter', letter)
+    #first eample
+var = 10
+
+while var > 0:
+    print('Current value : ', var)
+    var = var - 1
+    if var == 5:
+        break
+print("Good Bye")
+
+
+#Second Example
